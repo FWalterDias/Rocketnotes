472be3167724e83e297a20c02b4b527deccae056
Francisco Walter <franciscowalterd@gmail.com>
2024-02-10 03:16:07 -0300

    adicionando components de ButtonText, Input, e criação da interface Home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,42 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
+import { Header } from "../../components/Header";
+import { ButtonText } from "../../components/ButtonText";
+import { FiPlus, FiSearch } from "react-icons/fi";
+import { Input } from "../../components/Input";
+
+export function Home() {
+    return (
+        <Container>
+            <Brand>
+                <h1>Rocketnotes</h1>
+            </Brand>
+
+            <Header />
+
+            <Menu>
+                <li>
+                    <ButtonText title="Todos" isActive />
+                </li>
+                <li>
+                    <ButtonText title="React" />
+                </li>
+                <li>
+                    <ButtonText title="Node" />
+                </li>
+            </Menu>
+
+            <Search>
+                <Input placeholder="pesquisar" icon={FiSearch} />
+            </Search>
+
+            <Content>
+
+            </Content>
+
+            <NewNote>   
+                Criar Nota
+                <FiPlus />
+            </NewNote>
+        </Container>
+    )
+}
