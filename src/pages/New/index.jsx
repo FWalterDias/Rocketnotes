634aa74bcad0c6618880c849f14c32633f5525b0
Francisco Walter <franciscowalterd@gmail.com>
2024-02-13 13:21:28 -0300

    finalizando pagina de criar nota

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,51 @@
+import { Container, Form } from "./styles";
+import { Header } from "../../components/Header"
+import { Input } from "../../components/Input";
+import { Textarea } from "../../components/Textarea";
+import { NoteItem } from "../../components/NoteItem";
+import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
+
+export function New() {
+    return (
+        <Container>
+            <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a href="/">voltar</a>
+                    </header>
+
+                    <Input
+                        placeholder="titulo"
+                    />
+
+                    <Textarea
+                        placeholder="Observações"
+                    />
+
+                    <Section title="Links Uteis">
+                        <NoteItem
+                            value="https://www.google.com"
+                        />
+                        <NoteItem
+                            isNew
+                            placeholder="Novo Link"
+                        />
+                    </Section>
+
+                    <Section title="Marcadores">
+                        <div className="tags">
+                            <NoteItem value="react" />
+                            <NoteItem isNew placeholder="Nova Tag" />
+                        </div>
+                    </Section>
+
+                    <Button tittle="Salvar" />
+                </Form>
+            </main>
+        </Container>
+    )
+}
