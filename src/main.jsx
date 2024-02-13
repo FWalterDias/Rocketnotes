a2fd68959689ecaba55e1a78c60a73ff2b669649
Francisco Walter <franciscowalterd@gmail.com>
2024-02-13 12:25:56 -0300

    criando pagina de perfil

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme';
+import { Signup } from './pages/Signup';
 import GlobalStyle from './styles/global';
-import { Signin } from './pages/Signin';
+import theme from './styles/theme';
+import { Profile } from './pages/Profile';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Signin />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>,
 )
